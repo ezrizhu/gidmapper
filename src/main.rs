93ee1cebd2c11ca3b8c9d0e59d2857046fd97cf1
Eric Zhu <eric@ericz.me>
2023-06-19 23:52:47 -0400

    use threads

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::thread;
+
 fn main() {
-    let pid = std::env::args().nth(1).expect("no pid given");
-    let uid_path = format!("{}{}{}", "/proc/",pid,"/uid_map");
-    let gid_path = format!("{}{}{}", "/proc/",pid,"/gid_map");
-    std::fs::write(uid_path, "0 1000 1")
-        .expect("unable to write uid_map file");
-    std::fs::write(gid_path, "0 1000 65535")
-        .expect("unable to write gid_map file");
+    let handler = thread::spawn(|| {
+        let pid = std::env::args().nth(1).expect("no pid given");
+        let uid_path = format!("{}{}{}", "/proc/",pid,"/uid_map");
+        let gid_path = format!("{}{}{}", "/proc/",pid,"/gid_map");
+        std::fs::write(uid_path, "0 1000 1")
+            .expect("unable to write uid_map file");
+        std::fs::write(gid_path, "0 1000 65535")
+            .expect("unable to write gid_map file");
+        });
+    handler.join().unwrap();
 }
